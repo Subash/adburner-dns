7459c5e96b4d363519e7000ca775eae156d50465
Subash Pathak <subash@subash.me>
2019-02-22 13:27:35 +0545

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ const path = require('path');
 
 const env = Object.assign({}, {
   PORT: 53,
-  DNS_OVER_HTTPS: `true`, //process.env items are strings
-  HTTPS_REMOTE_ADDRESS: `https://cloudflare-dns.com/dns-query,https://dns.google.com/experimental`,
-  UDP_REMOTE_ADDRESS: `1.1.1.1,8.8.8.8`,
-  BLOCKED_HOSTS_URL: `https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts`,
-}, process.env); //Create new env by merging process.env and default values
+  DNS_OVER_HTTPS: true,
+  HTTPS_REMOTE_ADDRESS: 'https://cloudflare-dns.com/dns-query,https://dns.google.com/experimental',
+  UDP_REMOTE_ADDRESS: '1.1.1.1,8.8.8.8',
+  BLOCKED_HOSTS_URL: 'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
+}, process.env); //Merge process.env and defaults
 
 module.exports = {
-  useHttp: env.DNS_OVER_HTTPS == 'true',
-  httpServers: env.HTTPS_REMOTE_ADDRESS.split(',').map( item=> item.trim()), //Allow comma separated list
-  udpServers: env.UDP_REMOTE_ADDRESS.split(',').map( item=> item.trim()), //Allow comma separated list
+  useHttp: env.DNS_OVER_HTTPS == 'true', //Can be a string
+  httpServers: env.HTTPS_REMOTE_ADDRESS.split(',').map( item=> item.trim()), //Split comma separated lists
+  udpServers: env.UDP_REMOTE_ADDRESS.split(',').map( item=> item.trim()), //Split comma separated lists
   hostsUrl: env.BLOCKED_HOSTS_URL,
   timeout: 10 * 1000, // 10 Seconds
   cacheDir: path.resolve(__dirname, '../data'),
